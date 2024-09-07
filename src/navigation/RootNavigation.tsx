11e5477994616a2c396c29cfcddebab6a6a0e5c9
minhvh5504 <vihongminh5504@gmail.com>
2024-09-07 15:07:21 +0700

    update login screen

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -1,13 +1,8 @@
 import LoginScreen from '@views/LoginScreen'
 import React from 'react'
-import { SafeAreaView } from 'react-native'
 
 const RootNavigation = () => {
-  return (
-    <SafeAreaView>
-      <LoginScreen />
-    </SafeAreaView>
-  )
+  return <LoginScreen />
 }
 
 export default RootNavigation
